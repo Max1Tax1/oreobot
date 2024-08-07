b17a733ea7d40042902bea56ace85f6c2b7ebf5a
Max Zheng <88370897+Max1Tax1@users.noreply.github.com>
2024-08-07 14:00:34 +1000

    Create secrets.js template

---
+++ src/secrets.js
@@ -0,0 +1,12 @@
+/**
+ * IDs and tokens File, to be kept secret! Follow and fill the template.
+ */
+
+export const botToken = '' // Your bot's token
+export const botID = '' // Your bot's token
+export const testGuildID = '' // A server you want to test the bot on
+export const botEmojiGuildIDs = {
+    'music': '1252869262738067477',
+    'navigation': '1265159808323944480'
+}
+export const youtubeCookies = '' // Your youtube cookies
